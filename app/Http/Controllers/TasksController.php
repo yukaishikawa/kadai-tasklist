6d8db0c095143a291138c9afffd8ccea168e8fba
yuka.ishikawa <yuka.ishikawa@rakuten.com>
2018-06-05 00:17:33 +0000

    git update

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -62,7 +62,7 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        $message = Task::find($id);
+        $task = Task::find($id);
 
         return view('tasks.show', [
             'task' => $task,
@@ -77,7 +77,11 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::find($id);
+
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
     }
 
     /**
