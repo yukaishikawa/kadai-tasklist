f6566aedf0505cdf9ebe2bbea944d6902d85a88d
yuka.ishikawa <yuka.ishikawa@rakuten.com>
2018-06-07 03:50:31 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,19 +3,37 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
-
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+            <div class="row">
+                
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">　
+            
+            　　
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+          
+                {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                    
+                </div>
+                    
+                <div class="form-group">
+                
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            　　{!! Form::close() !!}
+            　　
+            　　
+                </div>
+            
+            </div>
 
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+        
 
 @endsection
