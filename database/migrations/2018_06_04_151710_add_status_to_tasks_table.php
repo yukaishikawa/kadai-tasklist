47c00711cf7d1b142a8b01991bd54789a7000787
yuka.ishikawa <yuka.ishikawa@rakuten.com>
2018-06-08 07:34:21 +0000

    modified

--- database/migrations/2018_06_04_151710_add_status_to_tasks_table.php
+++ database/migrations/2018_06_04_151710_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class AddTitleToTasksTable extends Migration
 {
     /**
      * Run the migrations.
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -21,7 +21,7 @@
                     
                 <div class="form-group">
                 
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
                 
