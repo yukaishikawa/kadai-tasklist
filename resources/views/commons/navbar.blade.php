2aefa82966c5d1772a33748aa3e2ab176be3b3aa
yuka.ishikawa <yuka.ishikawa@rakuten.com>
2018-06-14 03:27:07 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,12 +13,11 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Signup</a></li>
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
                     <li><a href="#">Login</a></li>
                     <li>{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>
                 </ul>
             </div>
-            
             </div>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-danger']) !!}
         </div>
     </div>
 @endsection
