b4d299d1909957bfc9e91d5b3896cddd1616b124
yuka.ishikawa <yuka.ishikawa@rakuten.com>
2018-06-18 04:01:57 +0000

    update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,7 +16,7 @@
             <tbody>
                 @foreach ($tasks as $task)
                     <tr>
-                        <td>{!! link_to_route('tasks.edit', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                         <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
